4d7425a79c8cbafcaac68c1c8804d9bebb53e8bb
VeljkoStojanovic <veljkostojanovicima@gmail.com>
2026-02-13 17:02:34 +0100

    env file, application properties and docker-compose fixed

--- src/main/java/com/webshop/encryption/SecurityConfig.java
+++ src/main/java/com/webshop/encryption/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
 
         http
                 .csrf(csrf -> csrf.disable())
