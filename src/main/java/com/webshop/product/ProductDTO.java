d9548abf2aa1942c3b533d5af9a33aab49013fed
VeljkoStojanovic <veljkostojanovicima@gmail.com>
2026-03-09 20:16:48 +0100

    x

--- src/main/java/com/webshop/product/ProductDTO.java
+++ src/main/java/com/webshop/product/ProductDTO.java
@@ -1,7 +1,6 @@
 package com.webshop.product;
 
 import com.webshop.category.CategoryDTO;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
