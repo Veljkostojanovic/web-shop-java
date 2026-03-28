b2bf502b2d0f240deaa867ee3b3db792faa5ad13
VeljkoStojanovic <veljkostojanovicima@gmail.com>
2026-03-28 14:31:57 +0100

    table names

--- backend/src/main/java/com/webshop/category/Category.java
+++ backend/src/main/java/com/webshop/category/Category.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "categories")
 public class Category {
 
     @Id
--- backend/src/main/java/com/webshop/product/Product.java
+++ backend/src/main/java/com/webshop/product/Product.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "products")
 public class Product {
 
     @Id
